e7dfe20bf51bf067e720ed0f84443a177f17266d
Juan Montero <juanca.montero76@gmail.com>
2023-10-31 07:09:29 -0500

    nextjs tutorial before database

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+type Props = {}
+export default function InvoicesPage({}: Props) {
+  return (
+    <div>InvoicesPage</div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css'
+import { orbitron } from './ui/fonts'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${orbitron.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
